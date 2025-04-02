2b431306836e240081e739193e961726ae2d6d8c
Maryna <marinailukova01@gmail.com>
2025-04-02 20:35:49 +0300

    update index

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-nameInput.addEventListener("input", (event) => {
-  const valueInput = event.target.value.trim();
-  nameOutput.textContent = valueInput === "" ? "Anonymous" : valueInput;
-});
